4c341367a853172f9114e80a2a4c6650f5818afc
Sushant Gupta <90310743+sushant2106@users.noreply.github.com>
2023-05-04 21:10:23 +0530

    Update Binary_to_Decimal.java

--- Binary_to_Decimal.java
+++ Binary_to_Decimal.java
@@ -1,8 +1,22 @@
 package org.example;
 import java.util.*;
-public class Binary_to_Decimal{
-    public static void main(String args[]){
-        int n=101;
+public class Main{
+     static void decimal_BInary(){
+        int n=5;
+        int pow=0;
+        int deci=0;
+
+        while(n>0){
+            int rem=n%2;
+            deci+=(rem*(int)Math.pow(10,pow++));
+            n=n/2;
+
+        }
+        System.out.println(deci);
+
+    }
+   static void binary_decimal(){
+           int n=101;
         int deci=0;
         int i=0;
         while(n>0){
@@ -11,6 +25,11 @@ public class Binary_to_Decimal{
             n=n/10;
         }
         System.out.println(deci);
+   }
+    public static void main(String args[]){
+    
+        binary_decimal();
+        decimal_Binary();
 
     }
 }
