b8ed8aa0ab30e48618358cef12cc075cc2ebf89c
Sushant Gupta <90310743+sushant2106@users.noreply.github.com>
2023-05-06 11:49:21 +0530

    Update Selection_Sort.java

--- Sorting/Selection_Sort.java
+++ Sorting/Selection_Sort.java
@@ -22,7 +22,26 @@ public class Main
 	}
 	
 	System.out.print(Arrays.toString(arr));
-		
+   /*
+   int arr[]={1,5,4,3,2};
+	int n=arr.length;
+	for(int i=0;i<n;i++){
+	   
+	    int index=i;
+    for(int j=i+1;j<n;j++)
+     {
+          if(arr[index]>arr[j]){
+          index=j;
+      }
+      int tem=arr[index];
+      arr[index]=arr[i];
+      arr[i]=tem;
+     }
+      
+	}
+	
+	System.out.print(Arrays.toString(arr));
+	*/
 		
 	}
 }
