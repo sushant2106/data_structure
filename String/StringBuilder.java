48b6e6abc3129f5132f78e8574f70daf23dccf8b
Sushant Gupta <90310743+sushant2106@users.noreply.github.com>
2023-06-15 07:54:23 +0530

    Create StringBuilder.java

---
+++ String/StringBuilder.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		String str="WNEENESENNN";
+	    StringBuilder sb=new StringBuilder("");
+	    for(int i=0;i<str.length();i++){
+	        sb.append(str.charAt(i));
+	    }
+	    System.out.println(sb);
+	}
+}
