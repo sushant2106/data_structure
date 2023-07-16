4333e92aee1d97a2e02b7db1b7a1528a49dda0a3
Sushant Gupta <90310743+sushant2106@users.noreply.github.com>
2023-07-16 10:09:04 +0530

    Update Sum of those elements is maximum(Sum of the subset is maximum) .java

--- DP/ONE-D/Sum of those elements is maximum(Sum of the subset is maximum) .java
+++ DP/ONE-D/Sum of those elements is maximum(Sum of the subset is maximum) .java
@@ -1,17 +1,9 @@
 import java.util.*;
-//Here we go :   Given an array of integers(positive as well as negative) ,select some elements from this array(select a subset) such that:-
 
-//1. Sum of those elements is maximum(Sum of the subset is maximum) .
-
-//2. No two elements in the subset should be consecutive.
-
-Example :- {2,4,6,7,8}
-
-Answer:- {2+6+8=16}
 public class Main
 {
 	public static void main(String[] args) {
-	  int arr[]={2,3,5,-8,7};
+	  int arr[]={2,-3,5,-8,7};
 	  int n=5;
 	  int dp[]=new int[n];
 	  
