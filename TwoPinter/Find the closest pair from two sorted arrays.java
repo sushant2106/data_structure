f74f155c9ff4df38559ba11bda11ebcbc9e259b3
Sushant Gupta <90310743+sushant2106@users.noreply.github.com>
2023-07-16 15:48:29 +0530

    Update Find the closest pair from two sorted arrays.java
    Given two sorted arrays and a number x, find the pair whose sum is closest to x and the pair has an element from each array. 
    We are given two arrays ar1[0…m-1] and ar2[0..n-1] and a number x, we need to find the pair ar1[i] + ar2[j] such that absolute value of (ar1[i] + ar2[j] – x) is minimum.
    Example: 
    
    Input:  ar1[] = {1, 4, 5, 7};
            ar2[] = {10, 20, 30, 40};
            x = 32      
    Output:  1 and 30
    
    Input:  ar1[] = {1, 4, 5, 7};
            ar2[] = {10, 20, 30, 40};
            x = 50      
    Output:  7 and 40

--- TwoPinter/Find the closest pair from two sorted arrays.java
+++ TwoPinter/Find the closest pair from two sorted arrays.java
@@ -1,49 +1,24 @@
 import java.util.*;
-
 public class Main
 {
 	public static void main(String[] args) {
-		int ar1[] ={1, 4, 5, 7};
-        int ar2[] ={10, 20, 30, 40};
-        int x=50;
-        int ans[]=new int[2];
-        int n=ar1.length;
-        int m=ar2.length;
-        int min=Integer.MAX_VALUE;
-        // for(int i=0;i<n;i++){
-        //    int l=0;
-        //    int h=m-1;
-        //    while(l<h){
-        //        int sum=ar1[i]+ar2[h];
-        //        int curr=Math.abs(sum-x);
-        //        if(min>curr){
-        //            ans[0]=ar1[i];
-        //            ans[1]=ar2[h];
-        //            l++;
-        //            h--;
-        //            min=curr;
-        //        }
-        //        else{
-        //            l++;
-        //        }
-        //    }
-        // }
-     int l=0;
-         int h=m-1;
-         while(l<n && h>=0){
-             int dif=Math.abs(ar1[l]+ar2[h]-x);
-             if(min>dif){
-                 ans[0]=ar1[l];
-                 ans[1]=ar2[h];
-                 min=dif;
-             }
-             if(ar1[l]+ar2[h]>x){
-                 h--;
-             }
-             else{
-                 l++;
-             }
-         }
-        System.out.println(Arrays.toString(ans));
+	  	int arr1[] ={1, 4, 5, 7};
+        int arr2[] ={10, 20, 30, 40};
+        int k=50;
+	  int l=0,h=arr2.length-1;
+	  int n=arr2.length;
+	  int ans[]=new int[2];
+	  while(l<n && h>=0){
+	      if(arr1[l]+arr2[h]<=k){
+	          ans[0]=arr1[l];
+	          ans[1]=arr2[h];
+	          l++;
+	      }
+	      else{
+	          h--;
+	      }
+	  }
+	  System.out.println(Arrays.toString(ans));
 	}
+}
 }
