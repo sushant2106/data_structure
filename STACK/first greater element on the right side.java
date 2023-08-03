305f119d69501c6bca2fd3a6769f822cfe35a15f
Sushant Gupta <90310743+sushant2106@users.noreply.github.com>
2023-08-03 08:49:50 +0530

    Create first greater element on the right side.java
    The Next greater Element for an element x is the first greater element on the right side of x in the array. Elements for which no greater element exist, consider the next greater element as -1.

---
+++ STACK/first greater element on the right side.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		int arr[]={6,8,0,1,3};
+		int n=arr.length;
+		int ans[]=new int[n];
+		ans[n-1]=-1;
+		Stack<Integer>st=new Stack<>();
+		st.push(arr[n-1]);
+		for(int i=n-2;i>=0;i--){
+		    while(!st.isEmpty() && arr[i]>st.peek()){
+		        st.pop();
+		    }
+		    ans[i]=st.isEmpty()?-1:st.peek();
+		    st.push(arr[i]);
+		}
+		System.out.println(Arrays.toString(ans));
+	}
+}
