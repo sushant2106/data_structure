bfbd74ff6d2db2cb31daf486d529c67eafdd30fb
Sushant Gupta <90310743+sushant2106@users.noreply.github.com>
2023-08-12 07:29:35 +0530

    Create PriorityQueue.java

---
+++ Queue/PriorityQueue/PriorityQueue.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+  PriorityQueue<Integer>pq=new PriorityQueue<>();
+  pq.offer(12);
+  pq.offer(3);
+  pq.offer(11);
+  
+  System.out.println(pq);
+	}
+}
